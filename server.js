bfd6b6cab3865dfac597345aa74623b8b2917eb7
JMSONNE <jordynmsonne@gmail.com>
2024-07-11 16:56:05 -0500

    added api routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 5000
 require('dotenv').config();
+const productRoutes = require('./API/products');
 
 app.use(express.json());
 
