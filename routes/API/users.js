93411ba7b34aa26c68efff313573fe62f42e0a59
JMSONNE <jordynmsonne@gmail.com>
2024-07-15 19:47:30 -0500

    added login route with bcrypt hashing and jwt

--- routes/API/users.js
+++ routes/API/users.js
@@ -2,6 +2,8 @@ const { PrismaClient } = require('@jordynmsonne/capstone-db');
 const prisma = new PrismaClient();
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Fetches all users
 router.get('/users', async (req, res) => {
@@ -58,13 +60,15 @@ router.delete('/user/:id', async (req, res) => {
 });
 
 // Creates a new user
-router.post('/user', async (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password, name, email, role } = req.body;
     try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt)
         const user = await prisma.user.create({
             data: {
                 username,
-                password,
+                password: hashedPassword,
                 name,
                 email,
                 role
@@ -79,5 +83,39 @@ router.post('/user', async (req, res) => {
         res.status(500).json({ error: "Internal server error." })
     }
 });
+
+// login API
+router.post('/login', async (req, res) => {
+
+
+    try {
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (passwordMatch) {
+            const payload = {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                name: user.name,
+                role: user.role,
+            };
+            const accessToken = jwt.sign(payload, process.env.SECRET_KEY, {
+                expiresIn: "10d",
+            });
+            res.status(200).json({ accessToken, userId: user.id });
+        } else {
+            res.status(401).json({ error: "Failed to login" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
 
 module.exports = router;
