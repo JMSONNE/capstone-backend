9195d1392944d860b4906ce9fdd660c2b48dcc0c
JMSONNE <jordynmsonne@gmail.com>
2024-07-25 04:11:15 -0500

    added code

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { authenticate } = require('../../middleware/authentication')
+require('dotenv').config();
+
 
 // Fetches all users
 router.get('/users', async (req, res) => {
@@ -93,7 +95,7 @@ router.post('/login', async (req, res) => {
         const { email, password } = req.body;
         const user = await prisma.user.findUnique({
             where: {
-                email,
+                email
             },
         });
         const passwordMatch = await bcrypt.compare(password, user.password);
@@ -105,12 +107,11 @@ router.post('/login', async (req, res) => {
                 name: user.name,
                 role: user.role,
             };
-            const accessToken = jwt.sign(payload, process.env.SECRET_KEY, {
-                expiresIn: "7d",
-            });
+            const accessToken = jwt.sign(payload, process.env.SECRET_KEY);
             res.status(200).json({ accessToken, userId: user.id });
         } else {
             res.status(401).json({ error: "Failed to login" })
+            console.log(`password match: ${passwordMatch}`)
         }
     } catch (error) {
         console.log(error)
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 5000
 require('dotenv').config();
 const applyRoutes = require('./routes')
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
 applyRoutes(app);
+
+
 
 
 app.listen(PORT, () => {
