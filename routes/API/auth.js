0b908bb1f4e053dca1e90251693268de30535c0f
JMSONNE <jordynmsonne@gmail.com>
2024-08-03 00:20:28 -0500

    added proc file

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -119,5 +119,34 @@ router.post('/login', async (req, res) => {
     }
 })
 
+router.patch('/user/:id', async (req, res) => {
+    const { username, password, name, email, role } = req.body;
+    try {
+        const { id } = req.params;
+        const user = await prisma.user.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                username,
+                password,
+                name,
+                email,
+                role
+            }
+        })
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found." })
+        }
+
+        res.status(200).json({ message: "User found.", user })
+        console.log(user);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal server error." })
+    }
+})
+
 
 module.exports = router;
