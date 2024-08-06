92bbfb1e5dafb58345bbc56af844f1f5ef175202
JMSONNE <jordynmsonne@gmail.com>
2024-08-05 22:57:49 -0500

    added cart APIs

--- routes/API/cart.js
+++ routes/API/cart.js
@@ -5,10 +5,39 @@ const router = express.Router();
 const { authenticate } = require('../../middleware/authentication')
 
 // add product to cart
-router.post('/user:id/cart', authenticate("USER", "ADMIN", "GUEST"), async (req, res) => {
+router.post('/user:id/cart', authenticate("USER", "ADMIN"), async (req, res) => {
     try {
-        const { userId, productId, quantity } = req.body;
-    } catch (error) {
+        const { userId, user, cartItems } = req.body;
 
+        const cart = await prisma.cart.create({
+            data: {
+                userId,
+                user,
+                cartItems
+            }
+        });
+        console.log(cart)
+        res.status(200).json(cart)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal server error." })
+    }
+})
+
+// Fetch the users cart
+router.get('/user:id/cart', authenticate("USER", "ADMIN"), async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const cart = await prisma.cart.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        console.log(cart)
+        res.status(200).json(cart)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal server error." })
     }
 })
